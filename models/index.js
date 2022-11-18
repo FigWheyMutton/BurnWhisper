fea9dc28c64d5a744c93ba6aa25621dbd393cd21
teakoVA <nleb641999@gmail.com>
2022-11-17 20:20:51 -0800

    did models and seeds

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require('./user');
+const Post = require('./post');
+const Vote = require('./vote');
+const Comment = require('./comment');
+
+module.exports = { User, Post, Vote, Comment };

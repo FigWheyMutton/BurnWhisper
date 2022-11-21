866c2ba8466ae0c333d91e7cad3c1f9df9d84f0c
teakoVA <nleb641999@gmail.com>
2022-11-21 15:36:35 -0800

    added more controller routes;

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
+
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
+const homeRoutes = require('./home-routes.js');
+const profileRoutes = require('./profile-routes.js');
+const searchRoutes = require('./search-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/profile', profileRoutes);
+router.use('/search', searchRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;

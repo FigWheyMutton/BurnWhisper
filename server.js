c996ba23fcc97de888446cc5c25e8026d450bbfc
Oraan Levi <oraanlevi1@gmail.com>
2022-11-27 20:18:41 -0800

    login/logout page, handlebar still working

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+const { Cookie } = require('express-session');
+const { session } = require('./utils/passport');
+
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+
+app.use(require('./controllers'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(require('./controllers'));
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});

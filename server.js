9dc4f7c6ccd73e1cc913f370c082f9a222a564ed
teakoVA <nleb641999@gmail.com>
2022-11-28 19:04:25 -0800

    debugged server, installed const sess sequilize

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
-const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+// helper function
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({helpers});
+
+// session (connects session to sequelize Database) --> authentication 
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(require('./controllers'));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));

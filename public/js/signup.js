8c1caf46c5daf44c341bd37a436373942f7f5bba
Oraan Levi <oraanlevi1@gmail.com>
2022-11-29 18:58:20 -0800

    login/logout!!

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,32 +1,26 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
+const signupFormHandler = async (event) => {
+  event.preventDefault();
 
-    // getting data from the form
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-    const bio = document.querySelector('#bio-signup').value.trim();
-
-    console.log (username, email, password, bio)
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-          method: 'post',
-          body: JSON.stringify({
-            username,
-            email,
-            password,
-            bio
-          }),
-          headers: { 'Content-Type': 'application/json' }
-        }); 
-    // check the response status
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  const bio = document.querySelector('#bio-signup').value.trim();
+console.log(username, email, password)
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password, bio }),
+      headers: { 'Content-Type': 'application/json' },
+    });
     if (response.ok) {
-        console.log('Log in success!');
-        document.location.replace('/login');
-      } else {
-        alert(response.statusText);
-      }
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
     }
-}
+  }
+};
 
-document.querySelector('#form-signup').addEventListener('submit', signupFormHandler); 
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);

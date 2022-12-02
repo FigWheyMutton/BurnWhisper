7ae4d46b613ea5f23b5ab835cd16a6219f42ef02
unknown <alexhsieh12@gmail.com>
2022-12-01 20:17:56 -0800

    working posts showing up

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,8 +1,16 @@
 const { Post } = require('../models');
 
-const postData = []
+const postData = [
+    {
+        post_text: "This is the name of the post",
+        user_id: 1,
+    },
+    {
+        post_text: "hello im alex",
+        user_id: 1,
+    }
+]
 
-const postData = require('./post.json')
 
 const seedPosts = () => Post.bulkCreate(postData);
 
